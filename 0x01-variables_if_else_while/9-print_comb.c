efade9668e20b4dc13841f5c1cc49a0f789ec084
Laptopcorei7 <buabengasare56@gmail.com>
2023-03-18 03:06:10 -0700

    This is a commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		putchar(i + '0');
+	putchar(i + '0');
 
-		if (i != 9)
-			putchar(',');
-			putchar(' ');
+	if (i != 9)
+	putchar(',');
+	putchar(' ');
 
 	putchar('\n');
 
