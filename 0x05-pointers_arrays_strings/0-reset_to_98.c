679ce6ce3b1ff649ba197a8086e47b227f5f2bd4
Laptopcorei7 <buabengasare56@gmail.com>
2023-03-27 18:37:29 -0700

    Thisis a commit" ~ " A "

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * updateValue - Updates the value pointed to by a pointer to an integer.
+ * main - Doing something
+ * @ptr: Pointer to an integer variable.
+ */
+void updateValue(int *ptr)
+{
+	*ptr = 98;
+}
+
+int main(void)
+{
+	int n = 42;
+	int *p = &n;
+
+	printf("The value of n is currently: %d\n", n);
+
+	updateValue(p);
+
+	printf("The value of n is now: %d\n", n);
+
+	return (0);
+}
